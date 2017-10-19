af19cfd36b5b50cf112a0a32c153a6ce71ee116d
Ned Lanon <ned.lanon@outlook.com>
2017-10-19 12:15:06 +0100

    Added UWP project

---
+++ Bikr/UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Bikr.UWP
+{
+    public sealed partial class MainPage 
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new Bikr.App());
+        }
+    }
+}
